1637f13c94cbbbe21c3b1587c9e23e7d72731f48
GergelyBoldogkoi <boldogkoig1@gmail.com>
2017-06-07 11:48:32 +0100

    Now trains with denoised 16x16 data

--- SpinVision/commands/files.py
+++ SpinVision/commands/files.py
@@ -12,22 +12,21 @@ def truncateRecording(source, start,end, dest):
     nicedata = handler.truncate(source, start,end,dest)
 
 
+# truncateRecording(basepath + "Pos3To1_lowAngle_16x16_denoised", 116915910, 118805148, basepath + "TrainingSamples/16x16/Pos3To1_lowAngle_16x16_denoised_Sample1")
+# truncateRecording(basepath + "Pos3To1_lowAngle_16x16_denoised", 124541359, 126442203, basepath + "TrainingSamples/16x16/Pos3To1_lowAngle_16x16_denoised_Sample2")
+# truncateRecording(basepath + "Pos3To1_lowAngle_16x16_denoised", 132288534, 134129110, basepath + "TrainingSamples/16x16/Pos3To1_lowAngle_16x16_denoised_Sample3")
 
-# truncateRecording(basepath + "Pos3To1_lowAngle_16x16", 116915910, 118805148, basepath + "Pos3To1_lowAngle_16x16_Sample1")
-# truncateRecording(basepath + "Pos3To1_lowAngle_16x16", 124541359, 126442203, basepath + "Pos3To1_lowAngle_16x16_Sample2")
-# truncateRecording(basepath + "Pos3To1_lowAngle_16x16", 132288534, 134129110, basepath + "Pos3To1_lowAngle_16x16_Sample3")
+# truncateRecording(basepath + "Pos3To5_lowAngle_16x16_denoised", 374545004, 377171417, basepath + "TrainingSamples/16x16/Pos3To5_lowAngle_16x16_denoised_Sample1")
+# truncateRecording(basepath + "Pos3To5_lowAngle_16x16_denoised", 381759446, 385325434, basepath + "TrainingSamples/16x16/Pos3To5_lowAngle_16x16_denoised_Sample2")
+# truncateRecording(basepath + "Pos3To5_lowAngle_16x16_denoised", 389753227, 391839810, basepath + "TrainingSamples/16x16/Pos3To5_lowAngle_16x16_denoised_Sample3")
 
 # traininDirs = []
-# traininDirs.append(basepath + "Pos3To5_lowAngle_Sample1")
-# destFile = basepath + "Pos3To5_lowAngle_Sample1_filtered"
+# traininDirs.append(basepath + "Pos3To1_lowAngle_denoised")
+# destFile = basepath + "Pos3To1_lowAngle_16x16_denoised"
 #
-# source = "/home/kavits/Project/SpinVision/SpinVision/resources/DVS Recordings/test/"
 #
-# handler.readData(source + "10xtestSampleLeft")
+# handler.downsample(traininDirs[0], destFile, 8)
 
-# handler.modeDownsample(traininDirs[0], destFile)
-
-# handler.downsample(traininDirs[1], destFile, 8)
 # handler.concatenate(traininDirs, 100000, filter="Sample", dest=destFile, save=False)
 # handler.speedUp(10, traininDirs[0] + "/Sample1", traininDirs[0] + "/10xSample1")
 # handler.speedUp(10, traininDirs[1] + "/Sample1", traininDirs[1] + "/10xSample1")
--- SpinVision/firstTraining.py
+++ SpinVision/firstTraining.py
@@ -8,12 +8,12 @@ import networkControl as control
 def trainWithWeightSource():
     path = "/home/kavits/Project/SpinVision/SpinVision/resources/DVS Recordings/TrainingSamples/16x16/"
     files = []
-    files.append(path + "Pos3To1_lowAngle_16x16_Sample1")
-    files.append(path + "Pos3To5_lowAngle_16x16_Sample1")
+    files.append(path + "Pos3To1_lowAngle_16x16_denoised_Sample1")
+    files.append(path + "Pos3To5_lowAngle_16x16_denoised_Sample1")
     path = "/home/kavits/Project/SpinVision/SpinVision/resources/NetworkWeights/test/"
     #files.append(path + "testUntrainedGaussian_1024x40")
     # this is not gonna plot, just there to see if an error is raised
-    weights = control.train(256, 16, 100, files[0], files[1], plot=True)#, weightSource=files[2])
+    weights = control.train(256, 16, 10, files[0], files[1], plot=True)#, weightSource=files[2])
     print weights
 
 trainWithWeightSource()
